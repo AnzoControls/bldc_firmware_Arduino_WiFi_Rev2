44f1c8246e85e8bdbb0c48574608767d2b299d78
ashraful-khan <ashraful@anzocontrols.com>
2022-03-14 12:56:56 +0530

    Motor Working, WiFi library added and the system working

---
+++ src/arduino_secrets.h
@@ -0,0 +1,7 @@
+/* #define SECRET_SSID "OficeBay";         // your network SSID (name)
+#define SECRET_PASS "Aarone@123";       // your network SSID (name) */
+
+
+
+#define SECRET_SSID "Google LLC";         // your network SSID (name)
+#define SECRET_PASS "1234567890";       // your network SSID (name)
